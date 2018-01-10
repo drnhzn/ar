c67d8f3ab953cc28af98c5c77c2c95d794e5300d
Darren Hizon <darren.hizon@nuworks.ph>
2018-01-10 11:43:03 +0800

    Fixing AutoPlay

---
+++ js/play.js
@@ -0,0 +1,7 @@
+(function ($, window, document) {
+    $(function () {
+        $(window).on('click', function(){
+            $('#test').play();
+        });
+    });
+}(window.jQuery, window, document));
